bcf7ad2d1e0a4056385822996bd18464d08e9076
CalebSweeney <ctsweeney@csuchico.edu>
2026-09-25 15:47:34 -0700

    Refactor input handling for color blindness selection
    Replaced static input prompts with a loop to allow multiple entries until an invalid type is entered. Removed redundant code for processing color blindness types.

--- main.cpp
+++ main.cpp
@@ -41,26 +41,32 @@ int main()
   int newR = 0;
   int newG = 0;
   int newB = 0;
+  bool thing = true;
 
-  cout << "Please enter R for Protanopia, G for Deutranopia, and B for Tritanopia:" << endl;
-  cin >> selectedType;
-  cout << "Please enter three numbers, for R, G, and B. (from 0-255, inclusive):" << endl;
-  cin >> r >> g >> b;
-
-
-  if(selectedType == 'R'){
-    protanopia(r, g, b, newR, newG, newB);
-    cout << "Protanopia R G B: " << newR << " " << newG << " " << newB << endl;
-  }else if(selectedType == 'G'){
-    deutranopia(r, g, b, newR, newG, newB);
-    cout << "Deutranopia R G B: " << newR << " " << newG << " " << newB << endl;
-  }else if(selectedType == 'B'){
-    tritanopia(r, g, b, newR, newG, newB);
-    cout << "Tritanopia R G B: " << newR << " " << newG << " " << newB << endl;
-  }else{
-    cout << selectedType << " is not a supported Color Blindness type, please try again." << endl;
-  }
-
+  do{
+    cout << "Please enter R for Protanopia, G for Deutranopia, and B for Tritanopia (Enter any other letter to exit the program):" << endl;
+    cin >> selectedType;
+    if(selectedType != 'R' && selectedType != 'G' && selectedType != 'B'){
+      cout << "You have exited the program :(" << endl;
+      thing = false;
+    }
+    if(thing){
+      cout << "Please enter three numbers, for R, G, and B. (from 0-255, inclusive):" << endl;
+      cin >> r >> g >> b;
+    }
+  
+  
+    if(selectedType == 'R'){
+      protanopia(r, g, b, newR, newG, newB);
+      cout << "Protanopia R G B: " << newR << " " << newG << " " << newB << endl;
+    }else if(selectedType == 'G'){
+      deutranopia(r, g, b, newR, newG, newB);
+      cout << "Deutranopia R G B: " << newR << " " << newG << " " << newB << endl;
+    }else if(selectedType == 'B'){
+      tritanopia(r, g, b, newR, newG, newB);
+      cout << "Tritanopia R G B: " << newR << " " << newG << " " << newB << endl;
+    }
+  }while(thing);
 
   return 0;
 }
